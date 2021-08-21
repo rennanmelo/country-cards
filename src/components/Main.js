71e13f8322998fc2c50041742eba83ce21b081d2
Rennan Melo <rennan.melo11@gmail.com>
2021-08-21 06:33:25 -0300

    Created base of the Main component

---
+++ src/components/Main.js
@@ -0,0 +1,5 @@
+const Main = ({ children }) => {
+  return <main className="container mx-auto p-4">{children}</main>;
+};
+
+export default Main;
--- src/pages/ReactCountriesPage.js
+++ src/pages/ReactCountriesPage.js
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
+import Main from "../components/Main";
 
 const ReactCountriesPage = () => {
   return (
     <>
       <Header>Countries cards</Header>
+      <Main>My main</Main>
     </>
   );
 };
