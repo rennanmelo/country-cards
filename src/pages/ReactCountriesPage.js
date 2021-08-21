51a77a59d92ee429a285ac82a3be84ec944fd145
Rennan Melo <rennan.melo11@gmail.com>
2021-08-21 06:42:08 -0300

    Imported countries data

--- src/pages/ReactCountriesPage.js
+++ src/pages/ReactCountriesPage.js
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import Main from "../components/Main";
+import { allCountries } from "../data/countries";
 
 const ReactCountriesPage = () => {
   return (
