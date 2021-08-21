11cd0fb08b34c188a676eb0091f06d6edcd66569
Rennan Melo <rennan.melo11@gmail.com>
2021-08-21 07:11:33 -0300

    Created handleChange functionality of the input

--- src/pages/ReactCountriesPage.js
+++ src/pages/ReactCountriesPage.js
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import Header from "../components/Header";
 import Main from "../components/Main";
 import TextInput from "../components/TextInput";
 import { allCountries } from "../data/countries";
 
 const ReactCountriesPage = () => {
+  const [countryFilter, setCountryFilter] = useState("");
+
+  const handleCountryFilterChange = (newCountryFilter) => {
+    setCountryFilter(newCountryFilter);
+  };
+
   return (
     <>
       <Header>Countries cards</Header>
       <Main>
-        My main
-        <TextInput />
+        <TextInput
+          id="inputCountryFilter"
+          labelDescription="Country name (with at least 3 characters)"
+          inputValue={countryFilter}
+          autoFocus
+          onInputChange={handleCountryFilterChange}
+        />
       </Main>
     </>
   );
