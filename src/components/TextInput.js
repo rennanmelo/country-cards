078e118d8160daaa14fa9d398a6197b756b81617
Rennan Melo <rennan.melo11@gmail.com>
2021-08-21 06:53:14 -0300

    Created InputText component

---
+++ src/components/TextInput.js
@@ -0,0 +1,33 @@
+const TextInput = ({
+  labelDescription = "My label:",
+  inputValue = "Input value default",
+  onInputChange = null,
+  id = "id_do_input_text",
+  autoFocus = false,
+}) => {
+  function handleInputChange({ currentTarget }) {
+    if (onInputChange) {
+      const newValue = currentTarget.value;
+      onInputChange(newValue);
+    }
+  }
+
+  return (
+    <div className="flex flex-col my-4">
+      <label className="text-sm mb-1" htmlFor={id}>
+        {labelDescription}
+      </label>
+
+      <input
+        autoFocus={autoFocus}
+        id={id}
+        className="border p-1"
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+    </div>
+  );
+};
+
+export default TextInput;
--- src/pages/ReactCountriesPage.js
+++ src/pages/ReactCountriesPage.js
@@ -1,12 +1,16 @@
 import Header from "../components/Header";
 import Main from "../components/Main";
+import TextInput from "../components/TextInput";
 import { allCountries } from "../data/countries";
 
 const ReactCountriesPage = () => {
   return (
     <>
       <Header>Countries cards</Header>
-      <Main>My main</Main>
+      <Main>
+        My main
+        <TextInput />
+      </Main>
     </>
   );
 };
